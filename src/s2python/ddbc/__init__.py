e2ece007ab9b9237e134193aa4161939a09e98dc
Vlad Iftime <vladiftime60@gmail.com>
2025-01-07 22:37:02 +0100

    Add DDBC actuator and operation mode classes

---
+++ src/s2python/ddbc/__init__.py
@@ -0,0 +1,2 @@
+from s2python.ddbc.ddbc_actuator_description import DDBCActuatorDescription
+
---
+++ src/s2python/ddbc/ddbc_operation_mode.py
@@ -0,0 +1,23 @@
+from typing import List
+import uuid
+
+from s2python.generated.gen_s2 import DDBCOperationMode as GenDDBCOperationMode
+
+from s2python.common.power_range import PowerRange
+from s2python.common.number_range import NumberRange
+
+from s2python.validate_values_mixin import (
+    catch_and_convert_exceptions,
+    S2Message,
+)
+
+
+@catch_and_convert_exceptions
+class DDBCOperationMode(GenDDBCOperationMode, S2Message["DDBCOperationMode"]):
+    model_config = GenDDBCOperationMode.model_config
+    model_config["validate_assignment"] = True
+
+    id: uuid.UUID = GenDDBCOperationMode.model_fields["id"]
+    power_ranges: List[PowerRange] = GenDDBCOperationMode.model_fields["power_ranges"]  # type: ignore[assignment]
+    supply_ranges: List[NumberRange] = GenDDBCOperationMode.model_fields["supply_ranges"]  # type: ignore[assignment]
+    abnormal_condition_only: bool = GenDDBCOperationMode.model_fields["abnormal_condition_only"]  # type: ignore[assignment]
