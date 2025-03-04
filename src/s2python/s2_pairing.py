bad86c998e3b08b60b2d0990d969d8f154a3a6f7
Maurice Hendrix <maurice.hendrix@tno.nl>
2025-03-04 15:27:00 +0100

    Added S2Pairing implementation

---
+++ src/s2python/s2_pairing.py
@@ -0,0 +1,86 @@
+import logging
+import uuid
+from typing import Tuple
+import requests
+
+from jwskate import JweCompact
+from jwskate.jwk.rsa import RSAJwk
+from binapy.binapy import BinaPy
+
+from s2python.generated.gen_s2_pairing import (Protocols,
+                                               PairingRequest,
+                                               S2NodeDescription,
+                                               PairingResponse,
+                                               ConnectionRequest,
+                                               ConnectionDetails)
+
+
+logger = logging.getLogger("s2python")
+
+class S2Pairing:  # pylint: disable=too-many-instance-attributes
+    paired: bool
+    s2_server_node_id: str
+    server_node_description: str
+    selected_protocol: Protocols
+    connection_uri: str
+    challenge: BinaPy
+
+    _request_pairing_endpoint: str
+    _token: str
+    _s2_client_node_description: S2NodeDescription
+    _verify_certificate: bool | str  # pylint: disable=E1131
+    _client_node_id: str
+    _supported_protocols: Tuple[Protocols]
+    _rsa_key_pair: RSAJwk
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        request_pairing_endpoint: str,
+        token: str,
+        s2_client_node_description: S2NodeDescription,
+        verify_certificate: bool | str = False,  # pylint: disable=E1131
+        client_node_id: str = str(uuid.uuid4()),
+        supported_protocols: Tuple[Protocols] = (Protocols.WebSocketSecure, )
+    ) -> None:
+        self.paired = False
+
+        self._request_pairing_endpoint = request_pairing_endpoint
+        self._token = token
+        self._s2_client_node_description = s2_client_node_description
+        self._verify_certificate = verify_certificate
+        self._client_node_id = client_node_id
+        self._supported_protocols = supported_protocols
+        self._rsa_key_pair = RSAJwk(self._rsa_key_pair)
+
+    def pair(self) -> bool:
+        self.paired = False
+        pairing_request: PairingRequest = PairingRequest(token=self._token,
+                                                        publicKey=self._rsa_key_pair.public_jwk().to_pem(),
+                                                        s2ClientNodeId=self._client_node_id,
+                                                        s2ClientNodeDescription=self._s2_client_node_description,
+                                                        supportedProtocols=self._supported_protocols)
+
+        response = requests.post(self._request_pairing_endpoint,
+                                 json=pairing_request.model_dump_json(),
+                                 timeout=10,
+                                 verify = self._verify_certificate)
+        response.raise_for_status()
+        pairing_response: PairingResponse = PairingResponse.parse_raw(response.json())
+        self.s2_server_node_id = pairing_response.s2ServerNodeId
+        self.server_node_description = pairing_response.serverNodeDescription
+
+        connection_request: ConnectionRequest = ConnectionRequest(s2ClientNodeId=self._client_node_id,
+                                                                   supportedProtocols=self._supported_protocols)
+
+        response = requests.post(pairing_response.requestConnectionUri,
+                                 json=connection_request.model_dump_json(),
+                                 timeout=10,
+                                 verify = self._verify_certificate)
+        response.raise_for_status()
+        connection_details: ConnectionDetails = ConnectionDetails.parse_raw(response.json())
+
+        self.selected_protocol = connection_details.selectedProtocol
+        self.connection_uri = connection_details.connectionUri
+        self.challenge = JweCompact(connection_details.challenge).decrypt(self._rsa_key_pair)
+
+        self.paired = True
+        return self.paired
