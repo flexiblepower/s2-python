1474d9c520cef42d08c5c40312a64bfc7bbc3e8e
F.N. Claessen <felix@seita.nl>
2025-03-18 11:02:07 +0100

    style: flake8 and black
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- tests/unit/message_test.py
+++ tests/unit/message_test.py
@@ -9,28 +9,36 @@ from s2python.validate_values_mixin import S2MessageComponent
 
 
 class S2MessageTest(unittest.TestCase):
+    """Check importing S2Message classes from s2_python.message."""
 
     def _test_import_s2_messages(self, module_name):
-        """Make sure each S2MessageComponent subclass in the given module is importable from s2_python.message."""
+        """Check each S2MessageComponent subclass in the given module is importable."""
         module = importlib.import_module(module_name)
 
         # Find all submodules
         all_subclasses = []
-        for _, name, is_pkg in pkgutil.iter_modules(module.__path__, module.__name__ + "."):
+        for _, name, is_pkg in pkgutil.iter_modules(
+            module.__path__, module.__name__ + "."
+        ):
             submodule = importlib.import_module(name)
 
             # Find all classes in the submodule that subclass BaseClass
             subclasses = [
-                obj for _, obj in inspect.getmembers(submodule, inspect.isclass)
+                obj
+                for _, obj in inspect.getmembers(submodule, inspect.isclass)
                 if issubclass(obj, S2MessageComponent) and obj is not S2MessageComponent
             ]
             all_subclasses.extend(subclasses)
 
         # Ensure we found at least one subclass
-        self.assertGreater(len(all_subclasses), 0, f"No subclasses found in {module_name}")
+        self.assertGreater(
+            len(all_subclasses), 0, f"No subclasses found in {module_name}"
+        )
 
         for S2MessageClass in all_subclasses:
-            assert hasattr(message, S2MessageClass.__name__), f"{S2MessageClass} should be importable from s2_python.message"
+            assert hasattr(
+                message, S2MessageClass.__name__
+            ), f"{S2MessageClass} should be importable from s2_python.message"
 
     def test_import_s2_messages__common(self):
         self._test_import_s2_messages("s2python.common")
