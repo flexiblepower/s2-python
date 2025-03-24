094173c0f394845f2d7d4e8b62af9b12356fe089
F.N. Claessen <felix@seita.nl>
2025-03-24 12:30:30 +0100

    style: pylint
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- tests/unit/message_test.py
+++ tests/unit/message_test.py
@@ -39,9 +39,15 @@ class S2MessageTest(unittest.TestCase):
                 message, _class.__name__
             ), f"{_class} should be importable from s2_python.message"
             if "message_id" in _class.model_fields:
-                assert _class in get_args(message.S2Message), f"{_class} should be typed as a s2_python.message.S2Message"
+                assert (
+                    _class in get_args(message.S2Message),
+                    f"{_class} should be typed as a s2_python.message.S2Message",
+                )
             else:
-                assert _class in get_args(message.S2MessageElement), f"{_class} should be typed as a s2_python.message.S2MessageElement"
+                assert (
+                    _class in get_args(message.S2MessageElement),
+                    f"{_class} should be typed as a s2_python.message.S2MessageElement",
+                )
 
     def test_import_s2_messages__common(self):
         self._test_import_s2_messages("s2python.common")
