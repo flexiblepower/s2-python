e72847e6e16815c267d5d7cb20acfe33e1ed2718
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2025-04-15 19:17:35 +0200

    Fix/use datamodel codegen (#105)
    * docs: add instruction for generating gen_s2_pairing.py
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    * feat: add generated classes for S2 pairing & authentication
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>
    
    ---------
    
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- ci/generate_s2.sh
+++ ci/generate_s2.sh
@@ -3,3 +3,4 @@
 
 . .venv/bin/activate
 datamodel-codegen  --input specification/openapi.yml --input-file-type openapi --output-model-type pydantic_v2.BaseModel --output src/s2python/generated/gen_s2.py --use-one-literal-as-default
+# datamodel-codegen  --input specification/s2-over-ip-pairing.yaml --input-file-type openapi --output-model-type pydantic_v2.BaseModel --output src/s2python/generated/gen_s2_pairing.py --use-one-literal-as-default
