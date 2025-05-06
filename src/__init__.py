8552f9ddadc14432df8bcec6a81711d281eefb7b
Vlad Iftime <vladiftime60@gmail.com>
2025-05-06 15:48:08 +0200

    Chore: Fix tests
    Signed-off-by: Vlad Iftime <vladiftime60@gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-import sys
-from s2python.communication.s2_connection import S2Connection
-sys.modules['s2python.s2_connection'] = sys.modules.get('s2python.communication.s2_connection', None)
+# import sys
+# from s2python.communication.s2_connection import S2Connection
+# sys.modules['s2python.s2_connection'] = sys.modules.get('s2python.communication.s2_connection', None)
