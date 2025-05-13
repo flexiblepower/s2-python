83ab15bcd8ff78f9922a794dc48c67d08a24684c
Andrew Rutherfoord <andrew.rutherfoord@tno.nl>
2025-05-13 11:12:00 +0200

    Added validation and tests for PowerForecastElement & PowerMeasurement

--- src/s2python/common/power_measurement.py
+++ src/s2python/common/power_measurement.py
@@ -1,8 +1,13 @@
 from typing import List
+from typing_extensions import Self
 import uuid
 
+from pydantic import model_validator
 from s2python.common.power_value import PowerValue
-from s2python.generated.gen_s2 import PowerMeasurement as GenPowerMeasurement
+from s2python.generated.gen_s2 import (
+    PowerMeasurement as GenPowerMeasurement,
+    CommodityQuantity,
+)
 from s2python.validate_values_mixin import (
     catch_and_convert_exceptions,
     S2MessageComponent,
@@ -16,3 +21,20 @@ class PowerMeasurement(GenPowerMeasurement, S2MessageComponent):
 
     message_id: uuid.UUID = GenPowerMeasurement.model_fields["message_id"]  # type: ignore[assignment,reportIncompatibleVariableOverride]
     values: List[PowerValue] = GenPowerMeasurement.model_fields["values"]  # type: ignore[assignment,reportIncompatibleVariableOverride]
+
+    @model_validator(mode="after")
+    def validate_values_at_most_one_per_commodity_quantity(self) -> Self:
+        """Validates the power measurement values to check that there is at most 1 PowerValue per CommodityQuantity."""
+
+        has_value: dict[CommodityQuantity, bool] = {}
+
+        for value in self.values:
+            if has_value.get(value.commodity_quantity, False):
+                raise ValueError(
+                    self,
+                    f"The measured PowerValues must contain at most one item per CommodityQuantity.",
+                )
+
+            has_value[value.commodity_quantity] = True
+
+        return self
