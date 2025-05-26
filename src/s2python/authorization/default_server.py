5593676c415dceac9c6166860648a76e7f220df7
F.N. Claessen <felix@seita.nl>
2025-05-26 15:08:35 +0200

    style: compatibility with Python 3.8
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- src/s2python/authorization/default_server.py
+++ src/s2python/authorization/default_server.py
@@ -9,7 +9,7 @@ import logging
 import socketserver
 import uuid
 from datetime import datetime
-from typing import Dict, Any, Tuple, Optional
+from typing import Dict, Any, Tuple, Optional, Union
 
 from jwskate import Jwk, Jwt
 from jwskate.jwe.compact import JweCompact
@@ -69,7 +69,7 @@ class S2DefaultHTTPHandler(http.server.BaseHTTPRequestHandler):
             logger.error("Error handling request: %s", e)
             raise e
 
-    def _send_json_response(self, status_code: int, response_body: dict | str) -> None:
+    def _send_json_response(self, status_code: int, response_body: Union[dict, str]) -> None:
         """
         Helper function to send a JSON response.
         :param handler: The HTTP handler instance (self).
