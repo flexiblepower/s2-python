7e0a16335365114f658b1499189c61486edaa75d
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2025-07-29 12:18:58 +0200

    str_enum: ombc classes should be explicitely exported in init.

--- src/s2python/ombc/__init__.py
+++ src/s2python/ombc/__init__.py
@@ -3,3 +3,11 @@ from s2python.ombc.ombc_operation_mode import OMBCOperationMode
 from s2python.ombc.ombc_status import OMBCStatus
 from s2python.ombc.ombc_system_description import OMBCSystemDescription
 from s2python.ombc.ombc_timer_status import OMBCTimerStatus
+
+__all__ = [
+    "OMBCInstruction",
+    "OMBCOperationMode",
+    "OMBCStatus",
+    "OMBCSystemDescription",
+    "OMBCTimerStatus",
+]
