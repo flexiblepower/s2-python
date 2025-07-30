048c54370da9093e85050d6140cfdecb00cc11d3
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2025-07-30 13:54:58 +0200

    Leverage str, enum capability of Python for any generated enums (#133)
    * str_enum: Change generation of S2 specification to include the subclass of an enum.
    
    * str_enum: Exclude generated files from pyright check.
    
    * str_enum: ombc classes should be explicitely exported in init.
    
    * str_enum: add missing Any to dict type in S2Parser.

--- ci/generate_s2.sh
+++ ci/generate_s2.sh
@@ -2,4 +2,4 @@
 
 
 . .venv/bin/activate
-datamodel-codegen  --input specification/openapi.yml --input-file-type openapi --output-model-type pydantic_v2.BaseModel --output src/s2python/generated/gen_s2.py --use-one-literal-as-default
+datamodel-codegen  --input specification/openapi.yml --input-file-type openapi --output-model-type pydantic_v2.BaseModel --output src/s2python/generated/gen_s2.py --use-one-literal-as-default --use-subclass-enum --output-datetime-class AwareDatetime --use-double-quotes
